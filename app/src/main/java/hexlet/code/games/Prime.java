e106995cb4b4d29d4a9a9c3f6ae71dc50d6869ad
AleksKen <aleks.ken2017@mail.ru>
2024-07-28 19:44:03 +0300

    fix

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -15,7 +15,7 @@ public class Prime {
     }
 
     private static boolean isPrime(int num) {
-        for (int i = 2; i <= Math.ceil(Math.sqrt(num)); i++) {
+        for (int i = 2; i <= Math.round(Math.sqrt(num)); i++) {
             if (num % i == 0) {
                 return false;
             }
